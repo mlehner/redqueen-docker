b9a929905712ef78b4a20fd97a761b3af6b0dbfd
Matt Lehner <mlehner@gmail.com>
2019-06-23 20:48:11 +0000

    remove extra logging, fix auth-proxy restart

--- auth-proxy/nginx-wrapper.sh
+++ auth-proxy/nginx-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "${HTPASSWD_CONTENTS}" > /.htpasswd
-envsubst '$SERVER_NAME' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
+envsubst '$SERVER_NAME' < /etc/nginx/conf.d/auth-proxy.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;' "$@"

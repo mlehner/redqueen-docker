f38c4d6b89a5299f4a045545e3e4da7882859774
Matt Lehner <matt.lehner@coachmeplus.com>
2022-08-14 18:27:35 -0400

    get rid of auth proxy

--- auth-proxy/nginx-wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "${HTPASSWD_CONTENTS}" > /.htpasswd
-envsubst '$SERVER_NAME' < /etc/nginx/conf.d/auth-proxy.conf.template > /etc/nginx/conf.d/default.conf
-
-exec nginx -g 'daemon off;' "$@"

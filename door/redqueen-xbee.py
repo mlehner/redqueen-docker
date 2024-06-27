0ad0fa1abb8bd56a1d218e8df522e5665178cb06
Matt Lehner <mlehner@gmail.com>
2024-06-27 19:08:28 -0300

    split on byte string

--- door/redqueen-xbee.py
+++ door/redqueen-xbee.py
@@ -42,7 +42,7 @@ while True:
                 passwd=mysql_pass,
                 db=mysql_database
             )
-            cmd, data = response['rf_data'].split(':', 1)
+            cmd, data = response['rf_data'].split(b':', 1)
             if cmd != 'A':
                 continue
 
